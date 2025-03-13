70a30dd8ce1134b442a248d1e7a6154afab9cd90
Alina Tumanova <alinatumanova2005@yandex.ru>
2025-03-13 19:56:41 +0300

    removed load dotenv from settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,7 @@ import os
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
 
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
